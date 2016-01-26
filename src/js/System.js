79598160c3e0ce01dd3566c9fb664b312de3da7d
Juuso Toikka <jtoikka@gmail.com>
2016-01-26 22:54:23 +0200

    Moved system to correct directory

--- src/js/System.js
+++ src/js/System.js
@@ -1,7 +1,11 @@
 import {log} from "Log";
 
 class System {
-
+  constructor() {
+    if (new.target === System) {
+      throw new TypeError("Cannot construct System instance directly");
+    }
+  }
   update(entities, delta) {
     entities.forEach((entity) => {
       this.update(entity.children);
