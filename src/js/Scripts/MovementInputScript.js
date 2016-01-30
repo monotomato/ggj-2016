35927a1a6458c683ca51404842bade182cc62960
Juuso Toikka <jtoikka@gmail.com>
2016-01-30 19:25:38 +0200

    Can toss in both directions

--- src/js/Scripts/MovementInputScript.js
+++ src/js/Scripts/MovementInputScript.js
@@ -13,9 +13,13 @@ class MovementInputScript extends Script {
     parent.entered = false;
     if (Input.keyDown.left) {
       movement -= this.movementSpeed;
+      parent.sprite.scale.x = -1;
+      parent.facingRight = false;
     }
     if (Input.keyDown.right) {
       movement += this.movementSpeed;
+      parent.sprite.scale.x = +1;
+      parent.facingRight = true;
     }
     if (Input.keyPressed.interact) {
       EventMan.publish({
--- src/js/Scripts/TossableScript.js
+++ src/js/Scripts/TossableScript.js
@@ -28,21 +28,18 @@ class TossableScript extends Script {
   handleGameEvent(parent, evt) {
     if (evt.eventType === 'interact_player') {
       if (this.picked) {
-        parent.physics.body.awake = true;
-        parent.physics.body.pos.x = parent.position.x;
-        parent.physics.body.pos.y = parent.position.y;
-        parent.physics.body.vel.x = 0.2;
+        let body = parent.physics.body;
+        body.awake = true;
+        body.pos.x = parent.position.x;
+        body.pos.y = parent.position.y;
+        console.log(parent);
+        if (this.player.facingRight) {
+          body.vel.x = 0.2;
+        } else {
+          body.vel.x = -0.2;
+        }
         parent.physics.body.vel.y = -0.2;
-        // parent.addPhysics('rectangle', {
-        //   pos: {
-        //     x: parent.position.x,
-        //     y: parent.position.y
-        //   },
-        //   vel: {
-        //     y: -0.01,
-        //     x: 0
-        //   }
-        // });
+
         console.log(parent.physics);
         this.picked = false;
       } else if (Collision.aabbTestFast(parent.physics.body, this.player.physics.body)) {
