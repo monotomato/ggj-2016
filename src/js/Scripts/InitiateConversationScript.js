4f9363caba2e9aa219a3d477007af20ce3dff751
Juuso Toikka <jtoikka@gmail.com>
2016-01-31 03:40:34 +0200

    Added conversation script

---
+++ src/js/Scripts/InitiateConversationScript.js
@@ -0,0 +1,48 @@
+import {log} from 'Log';
+import {Script} from 'Script';
+import {InputMan as Input} from 'Managers/InputManager';
+import {EventMan} from 'Managers/EventManager';
+import cfg from 'config.json';
+import {Factory} from 'Factory';
+
+class InitiateConversationScript extends Script {
+  constructor(parameters) {
+    super(parameters);
+    this.converse = false;
+    this.text = 'Lorem ipsum, yada yada. Game jam, peanutbutter.';
+    this.bubble = Factory.createSpeechBubble(11, 3, 6, this.text);
+  }
+
+  init(parent, rootEntity) {
+    this.player = rootEntity.findEntityWithTag('player');
+    parent.addChild(this.bubble);
+    this.bubble.visible = false;
+  }
+
+  update(parent, rootEntity, delta) {
+    if (this.converse) {
+      this.bubble.visible = true;
+    } else {
+      this.bubble.visible = false;
+    }
+  }
+
+  handleGameEvent(parent, evt) {
+    if (evt.eventType === 'change_text') {
+      if (evt.parameters.target == this) {
+        this.text = evt.parameters.text;
+        this.bubble.setText(this.text);
+      }
+    } else if (evt.eventType === 'start_conversation') {
+      if (evt.parameters.target == this) {
+        this.converse = false;
+      }
+    } else if (evt.eventType === 'end_conversation') {
+      if (evt.parameters.target == this) {
+        this.converse = true;
+      }
+    }
+  }
+}
+
+export {InitiateConversationScript};
