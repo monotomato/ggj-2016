cb955fb9a6076518ab97d9c7db091752239a0586
Juuso Toikka <jtoikka@gmail.com>
2016-01-31 14:36:26 +0200

    Moved bubbles higher

--- src/js/Scripts/RankBoardScript.js
+++ src/js/Scripts/RankBoardScript.js
@@ -16,6 +16,7 @@ class RankBoardScript extends Script {
     this.converse = false;
     this.text = 'This is placeholder text. Change it with events!';
     this.bubble = Factory.createSpeechBubble(13, 7, 6, this.text, 'Rankings', false);
+    this.bubble.position.y -= 40;
   }
 
   init(parent, rootEntity) {
